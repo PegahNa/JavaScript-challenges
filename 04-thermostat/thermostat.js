c22dae33ba84526ca8829624a8f02bf55955b22f
PegahNa <pegah_uk@yahoo.co.uk>
2023-06-15 16:10:17 +0100

    thermostat setTemperature and different ttype of usage added

--- 04-thermostat/thermostat.js
+++ 04-thermostat/thermostat.js
@@ -1,11 +1,25 @@
 class Thermostat {
   constructor() {
     this.temperature = 20;
+    this.powerSaving = true;
   }
 
   up() {
-    // if (power_mode === true && this.temperature < 25 || )
-    this.temperature += 1;
+    if (this.powerSaving === true && this.temperature < 25) {
+      this.temperature += 1;
+    } else if (this.powerSaving === false && this.temperature < 32) {
+      this.temperature += 1;
+    }
+  }
+
+  down() {
+    if (this.temperature > 10) {
+      this.temperature -= 1;
+    }
+  }
+
+  reset() {
+    this.temperature = 20;
   }
 
   getTemperature() {
@@ -15,6 +29,23 @@ class Thermostat {
   setTemperature(temperature) {
     this.temperature = temperature;
   }
-}
 
+  lowUsage() {
+    if (this.temperature < 18) {
+      console.log("low Usage");
+    }
+  }
+
+  mediumUsage() {
+    if (this.temperature > 18 && this.temperature < 25) {
+      console.log("medium Usage");
+    }
+  }
+
+  highUsage() {
+    if (this.temperature > 25) {
+      console.log("high Usage");
+    }
+  }
+}
 module.exports = Thermostat;
--- 04-thermostat/thermostat.test.js
+++ 04-thermostat/thermostat.test.js
@@ -6,4 +6,36 @@ describe("Thermostat", () => {
     new_thermostat.getTemperature();
     expect(new_thermostat.getTemperature()).toEqual(20);
   });
+
+  it("returns higher temperature 1 level", () => {
+    const new_thermostat = new Thermostat();
+    new_thermostat.up();
+    expect(new_thermostat.getTemperature()).toEqual(21);
+  });
+
+  it("returns higher temperature two levels", () => {
+    const new_thermostat = new Thermostat();
+    new_thermostat.up();
+    new_thermostat.up();
+    expect(new_thermostat.getTemperature()).toEqual(22);
+  });
+
+  it("returns lower temperature one levels", () => {
+    const new_thermostat = new Thermostat();
+    new_thermostat.down();
+    expect(new_thermostat.getTemperature()).toEqual(19);
+  });
+
+  it("returns lower temperature two levels", () => {
+    const new_thermostat = new Thermostat();
+    new_thermostat.down();
+    new_thermostat.down();
+    expect(new_thermostat.getTemperature()).toEqual(18);
+  });
+
+  it("returns the temperature to 20", () => {
+    const new_thermostat = new Thermostat();
+    new_thermostat.reset();
+    expect(new_thermostat.getTemperature()).toEqual(20);
+  });
 });
